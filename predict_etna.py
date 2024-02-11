0cb61a8e31e6642a769d47b061b83fccbb0ed4e0
Dim <dim@dims-macbook-pro.local>
2024-02-11 12:56:04 +0300

    Initial commit

--- predict_etna.py
+++ predict_etna.py
@@ -20,6 +20,7 @@ df_corrected_filtered_transposed = get_df_corrected_filtered_transposed()
 df_long = pd.melt(df_corrected_filtered_transposed.reset_index(), id_vars=['timestamp'],
                   var_name='segment', value_name='value')
 
+<<<<<<< HEAD
 
 print(df_long.head())
 print(df_long.dtypes)
@@ -35,6 +36,28 @@ ts = TSDataset(df=df_long,freq='M')
 # Определение трансформаций
 transforms = [
     LagTransform(in_column='volume', lags=[1, 3, 6]),
+=======
+df_long['target'] = df_long['value']
+#df_long['feature'] = 0
+
+df_long_ts_format = TSDataset.to_dataset(df_long)
+print(df_long.head())
+print(df_long.dtypes)
+
+print(df_long_ts_format.head())
+print(df_long_ts_format.dtypes)
+
+
+# Создание TSDataset
+
+ts = TSDataset(df=df_long_ts_format,freq='MS')
+
+ts.plot()
+
+# Определение трансформаций
+transforms = [
+    LagTransform(in_column='target', lags=[1, 3, 6]),
+>>>>>>> a3980dc (Initial commit)
     DateFlagsTransform()
 ]
 
@@ -43,6 +66,7 @@ model = LinearPerSegmentModel()
 
 # Создание и обучение пайплайна
 pipeline = Pipeline(model=model, transforms=transforms)
+<<<<<<< HEAD
 pipeline.fit(ts)
 
 # Прогнозирование
@@ -51,3 +75,13 @@ forecast = pipeline.forecast(future_ts)
 
 # Визуализация результатов
 plot_backtest(ts, forecast, model=model)
+=======
+# pipeline.fit(ts)
+#
+# # Прогнозирование
+# future_ts = ts.make_future(12)  # Прогноз на 12 месяцев вперёд
+# forecast = pipeline.forecast(future_ts)
+#
+# # Визуализация результатов
+# plot_backtest(ts, forecast, model=model)
+>>>>>>> a3980dc (Initial commit)
