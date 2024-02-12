f882a22310dc5fccae0b8dadbc2feb6045700f8d
dim <dmtrch.cd@gmail.com>
2024-02-12 14:02:09 +0300

    Initial commit

--- predict_etna.py
+++ predict_etna.py
@@ -11,36 +11,30 @@ from read_data_statistic import get_df_corrected_filtered_transposed
 # Предполагается, что df_corrected_filtered_transposed уже загружен и содержит необходимые временные ряды
 
 # Создание экземпляра TSDataset для целевого ряда и экзогенных переменных
-df_corrected_filtered_transposed = get_df_corrected_filtered_transposed()
+df_corr_fil = get_df_corrected_filtered_transposed()
+
 
 
 # После транспонирования и всех предыдущих преобразований, где 'timestamp' является индексом
 #df_corrected_filtered_transposed.reset_index(inplace=True)  # Сброс индекса, чтобы 'timestamp' стал столбцом
 
-df_long = pd.melt(df_corrected_filtered_transposed.reset_index(), id_vars=['timestamp'],
-                  var_name='segment', value_name='value')
-
-<<<<<<< HEAD
+df_long = pd.melt(df_corr_fil.reset_index(), id_vars=['timestamp'],
+                  var_name='segment', value_name='target')
 
 print(df_long.head())
 print(df_long.dtypes)
 
 
+df_long_ts_format = TSDataset.to_dataset(df_long)
 # Создание TSDataset
 
-ts = TSDataset(df=df_long,freq='M')
-
-
-# ts = TSDataset.to_dataset(df_corrected_filtered_transposed)
+ts = TSDataset(df=df_long_ts_format,freq='MS')
 
 # Определение трансформаций
 transforms = [
-    LagTransform(in_column='volume', lags=[1, 3, 6]),
-=======
-df_long['target'] = df_long['value']
-#df_long['feature'] = 0
+    LagTransform(in_column='target', lags=[1, 3, 6]),
+]
 
-df_long_ts_format = TSDataset.to_dataset(df_long)
 print(df_long.head())
 print(df_long.dtypes)
 
@@ -48,16 +42,9 @@ print(df_long_ts_format.head())
 print(df_long_ts_format.dtypes)
 
 
-# Создание TSDataset
-
-ts = TSDataset(df=df_long_ts_format,freq='MS')
-
-ts.plot()
-
 # Определение трансформаций
 transforms = [
     LagTransform(in_column='target', lags=[1, 3, 6]),
->>>>>>> a3980dc (Initial commit)
     DateFlagsTransform()
 ]
 
@@ -66,16 +53,6 @@ model = LinearPerSegmentModel()
 
 # Создание и обучение пайплайна
 pipeline = Pipeline(model=model, transforms=transforms)
-<<<<<<< HEAD
-pipeline.fit(ts)
-
-# Прогнозирование
-future_ts = ts.make_future(12)  # Прогноз на 12 месяцев вперёд
-forecast = pipeline.forecast(future_ts)
-
-# Визуализация результатов
-plot_backtest(ts, forecast, model=model)
-=======
 # pipeline.fit(ts)
 #
 # # Прогнозирование
@@ -84,4 +61,3 @@ plot_backtest(ts, forecast, model=model)
 #
 # # Визуализация результатов
 # plot_backtest(ts, forecast, model=model)
->>>>>>> a3980dc (Initial commit)
